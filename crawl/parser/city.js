5de03795616128a713fd381436c4de71b6be5273
groot <328592171@qq.com>
2018-11-23 19:04:12 +0800

    user

--- crawl/parser/city.js
+++ crawl/parser/city.js
@@ -1,6 +1,6 @@
 const puppeteer = require('puppeteer')
 const { timeout, log } = require('../utils/tools'); // 引入工具类.
-const UserParser = require('./user');
+const scrape = require('./user');
 
 class CityParser {
     static Run() {
@@ -9,45 +9,47 @@ class CityParser {
                 args: ['--no-sandbox'],
                 dumpio: false
             })
-            log('服务正常启动')
+            log('浏览器正常启动')
             try {
                 const page = await browser.newPage()
 
-                await page.goto('http://www.zhenai.com/zhenghun')
-                log('页面初次加载完毕')
+                await page.goto('http://www.zhenai.com/zhenghun', {
+                    waitUntil: 'networkidle2'
+                })
+                log('珍爱网城市页面加载完毕')
 
 
                 const handleData = async() => {
                     const list = await page.evaluate(() => {
 
-                            const cityList = []
+                        const cityList = []
 
-                            let itemList = document.querySelectorAll('.city-list dd a')
+                        let itemList = document.querySelectorAll('.city-list dd a')
 
-                            for (let item of itemList) {
-                                let cityData = {
-                                    city: undefined,
-                                    link: undefined,
-                                }
-                                cityData.link = item.href
-
-                                cityData.city = item.innerText
-
-                                cityList.push(cityData)
+                        for (let item of itemList) {
+                            let cityData = {
+                                name: undefined,
+                                link: undefined,
                             }
-                            return cityList
+                            cityData.link = item.href
 
-                        })
-                        // const result = await mongo.insertMany('GTX1080', list)
+                            cityData.name = item.innerText
 
+                            cityList.push(cityData)
+                        }
+                        return cityList
+
+                    })
                     return list
                 }
 
                 const result = await handleData()
                 console.log(result, '结果')
-                UserParser.Run(result)
+                for (let index = 0; index < 2; index++) {
+                    scrape(result[index])
+                }
                 await browser.close()
-                log('服务正常结束')
+                log('珍爱网城市页面浏览器关闭')
             } catch (error) {
                 console.log(error)
                 log('服务意外终止')
--- crawl/parser/user.js
+++ crawl/parser/user.js
@@ -1,64 +1,45 @@
 const puppeteer = require('puppeteer')
 const { timeout, log } = require('../utils/tools'); // 引入工具类.
+const elasticsearch = require('elasticsearch');
 
-class UserParser {
+let scrape = async(city) => {
+    const browser = await puppeteer.launch({
+        args: ['--no-sandbox'],
+        dumpio: false
+    })
+    log(city.name + ' - 浏览器正常启动')
+    for (let i = 1; i <= 5; i++) {
+        const page = await browser.newPage()
+        await page.goto(city.link + '/nv/' + i, {
+            waitUntil: 'networkidle2'
+        })
+        log(city.name + ' - 页面加载完毕')
+        var esClient = new elasticsearch.Client({
+            host: 'localhost:9200',
+            log: 'error'
+        });
 
-    static Run(list) {
-        (async() => {
-            const browser = await puppeteer.launch({
-                args: ['--no-sandbox'],
-                dumpio: false
-            })
-            log('服务正常启动')
-            for (let index = 0; index < list.length; index++) {
-                const page = await browser.newPage()
-                await page.goto(list[index].link)
-                log(list[index].city + '页面初次加载完毕')
-                timeout(500)
-                    // 使用一个 for await 循环，不能一个时间打开多个网络请求，这样容易因为内存过大而挂掉
-                for (let i = 1; i <= 5; i++) {
-                    // 找到分页的输入框以及跳转按钮
-                    const next = await page.$$(`.paging-item`)
-                    console.log(next[0], '介绍')
-                        // 模拟点击跳转
-                        // await next.click()
-                        // 等待页面加载完毕，这里设置的是固定的时间间隔，之前使用过page.waitForNavigation()，但是因为等待的时间过久导致报错（Puppeteer默认的请求超时是30s,可以修改）,因为这个页面总有一些不需要的资源要加载，而我的网络最近日了狗，会导致超时，因此我设定等待2.5s就够了
-                    await page.waitFor(2500)
-
-                    // 处理数据，这个函数的实现在下面
-                    await handleData()
-                        // 一个页面爬取完毕以后稍微歇歇，不然太快淘宝会把你当成机器人弹出验证码（虽然我们本来就是机器人）
-                    await page.waitFor(2500)
-                }
-
-                console.log(result, '结果')
+        const lists = await page.evaluate(() => {
+            const userList = []
+            let itemList = document.querySelectorAll('.introduce')
+            for (let item of itemList) {
+                userList.push({
+                    item: item.innerText
+                })
             }
-
-            const handleData = async() => {
-                const list = await page.evaluate(() => {
-
-                        const userList = []
-
-                        let itemList = document.querySelectorAll('.list-item')
-
-                        for (let item of itemList) {
-                            let cityData = {
-                                introduce: undefined,
-                            }
-                            cityData.introduce = item.querySelector('.introduce').innerText
-
-                            console.log(cityData.introduce, '介绍')
-
-                        }
-                        return cityList
-
-                    })
-                    // const result = await mongo.insertMany('GTX1080', list)
-
-                return list
-            }
-        })()
+            return userList
+        })
+        console.log(lists, '结果')
+        for (let index = 0; index < lists.length; index++) {
+            esClient.create({
+                index: 'myindex',
+                type: 'mytype',
+                id: index,
+                body: lists[index]
+            }, function(error, response) {});
+        }
     }
+    await browser.close()
 }
 
-module.exports = UserParser
+module.exports = scrape
